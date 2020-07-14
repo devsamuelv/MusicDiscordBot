c0c1686378ea5b620a3420b52d7d987386f7637d
DevSamuelV <realalloutmusic@gmail.com>
2020-07-14 00:08:15 -0400

    removed try { }

--- src/Message.java
+++ src/Message.java
@@ -12,54 +12,50 @@ public class Message extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        try {
-            Embeds embeds = new Embeds();
-            MessageChannel channel = event.getChannel();
-            String rawMessage = event.getMessage().getContentRaw();
-            String[] message = rawMessage.substring(prefix.length(), rawMessage.length()).split(" ");
+        Embeds embeds = new Embeds();
+        MessageChannel channel = event.getChannel();
+        String rawMessage = event.getMessage().getContentRaw();
+        String[] message = rawMessage.substring(prefix.length(), rawMessage.length()).split(" ");
 
-            if (!rawMessage.substring(0, 1).contains("%")) {
-                return;
-            }
+        if (!rawMessage.substring(0, 1).contains("%")) {
+            return;
+        }
 
-            switch (message[0]) {
-                case "hello":
-                    System.out.println(message);
-                    channel.sendMessage("Hello " + event.getAuthor().getName()).queue();
-                    break;
+        switch (message[0]) {
+            case "hello":
+                System.out.println(message);
+                channel.sendMessage("Hello " + event.getAuthor().getName()).queue();
+                break;
 
-                case "help":
-                    channel.sendMessage("Commands: %play, %disconnect, %pause, %chill, %trap, %edm").queue();
-                    break;
+            case "help":
+                channel.sendMessage("Commands: %play, %disconnect, %pause, %chill, %trap, %edm").queue();
+                break;
 
-                case "disconnect":
-                    channel.sendMessage("Not Finished").queue();
-                    break;
+            case "disconnect":
+                channel.sendMessage("Not Finished").queue();
+                break;
 
-                case "pause":
-                    channel.sendMessage("Not Finished").queue();
-                    break;
+            case "pause":
+                channel.sendMessage("Not Finished").queue();
+                break;
 
-                case "chill":
-                    channel.sendMessage("Not Finished").queue();
-                    break;
+            case "chill":
+                channel.sendMessage("Not Finished").queue();
+                break;
 
-                case "trap":
-                    channel.sendMessage("Not Finished").queue();
-                    break;
+            case "trap":
+                channel.sendMessage("Not Finished").queue();
+                break;
 
-                case "edm":
-                    channel.sendMessage("Not Finished").queue();
-                    channel.sendMessage(embeds.NotFinishedEmbed()).queue();
-                    break;
+            case "edm":
+                channel.sendMessage("Not Finished").queue();
+                channel.sendMessage(embeds.NotFinishedEmbed()).queue();
+                break;
 
-                default:
-                    channel.sendMessage("Commands: %play, %disconnect, %pause, %chill, %trap, %edm").queue();
-                    break;
+            default:
+                channel.sendMessage("Commands: %play, %disconnect, %pause, %chill, %trap, %edm").queue();
+                break;
 
-            }
-        } catch (Exception e) {
-            System.out.println(e.getCause());
         }
     }
 }
